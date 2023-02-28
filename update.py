a8245e32a09bfc78ee87a2500f9a71c9687baca0
zhangzm0128 <zhangzm0128@163.com>
2023-02-28 17:22:33 +0900

    Add files via upload

--- update.py
+++ update.py
@@ -6,9 +6,11 @@ from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import splitname, getnames
 import datetime
 from bs4 import BeautifulSoup 
+import re
 
 from pylatexenc.latex2text import LatexNodes2Text
 
+# load bib file 
 parser = BibTexParser(ignore_nonstandard_types = False)
 github_href_root = 'https://raw.githubusercontent.com/toyamaailab/toyamaailab.github.io/main/resource/'
 test_bib_path = './config/AI_SCIPapers_SCI.bib'
@@ -19,21 +21,31 @@ bib_database = bibtexparser.load(test_bib_file, parser)
 press_list = [x for x in bib_database.entries if x['ENTRYTYPE'] != 'inpress' ]
 inpress_list = [x for x in bib_database.entries if x['ENTRYTYPE'] == 'inpress']
 
+# read demo html file 
 publications_demo_html = BeautifulSoup(open('./config/publications_demo.html', 'r'), from_encoding='utf-8')
+highlights_demo_html = BeautifulSoup(open('./config/highlights_demo.html', 'r'), from_encoding='utf-8')
 data_demo_html = BeautifulSoup(open('./config/sourcedata_demo.html', 'r'), from_encoding='utf-8')
+
 index_html = BeautifulSoup(open('index.html', 'r'), from_encoding='utf-8')
 
+# update date to html
 publication_date = publications_demo_html.find(id="update_date")
 publication_date.string = 'Update: ' + str(datetime.date.today())
 
 data_date = data_demo_html.find(id="update_date")
 data_date.string = 'Update: ' + str(datetime.date.today())
 
+highlights_date = highlights_demo_html.find(id="update_date")
+highlights_date.string = 'Update: ' + str(datetime.date.today())
+
+
 index_date = index_html.find(id="update_date")
 index_date.string = 'Update: ' + str(datetime.date.today())
 with open("index.html", "w", encoding='utf-8') as file:
     file.write(index_html.decode('utf8'))
 
+
+# utils functions
 
 press_list = sorted(press_list, key=lambda x: datetime.datetime.strptime('{} {}'.format(x['month'], x['year']), '%B %Y'))[::-1]
 
@@ -132,6 +144,12 @@ def generate_note(bib, demo_html):
     if 'note' in bib:
         note_tag.append(' {} '.format(bib['note']))
     return note_tag
+    
+def is_highlight(bib, demo_html):
+    if 'highlight' in bib:
+        if bib['highlight'].casefold() == 'true'.casefold():
+            return True
+    return False
   
 def generate_doi_pdf(bib, demo_html):
     doi_pdf_tag = demo_html.new_tag('span', attrs={'class': 'doi'})
@@ -172,7 +190,10 @@ def generate_other_resource(bib, demo_html):
         return_string += ', [Code]'
         other_resource = True
         # resource_tag.append(', ')
-        code_tag =  demo_html.new_tag('a', attrs={'href': github_href_root + bib['code'], 'class': 'code', 'target': '_blank'})
+        if re.match(r'^https?:/{2}\w.+$', bib['code']):
+            code_tag = demo_html.new_tag('a', attrs={'href': bib['code'], 'class': 'code', 'target': '_blank'})
+        else:
+            code_tag =  demo_html.new_tag('a', attrs={'href': github_href_root + bib['code'], 'class': 'code', 'target': '_blank'})
         code_tag.string = '[Code]'
         resource_tag.append(code_tag)
     if 'resourcebaidu' in bib:
@@ -199,7 +220,7 @@ def generate_other_resource(bib, demo_html):
     
     return resource_tag
 
-
+# write inpress paper to publications html
 inpress_node = publications_demo_html.find(id="inpress") 
 for x in inpress_list:
     print(x['ID'])
@@ -228,7 +249,7 @@ for x in inpress_list:
     br_tag = publications_demo_html.new_tag('br')
     inpress_node.append(br_tag)
 
-    
+# write press paper to publications html
 press_node = publications_demo_html.find(id="published") 
 for x in press_list:
     print(x['ID'])
@@ -257,10 +278,11 @@ for x in press_list:
     br_tag = publications_demo_html.new_tag('br')
     press_node.append(br_tag)
 
+# generate publication html
 with open("publications.html", "w", encoding='utf-8') as file:
     file.write(publications_demo_html.decode('utf8'))
 
-
+# write the paper with data to sourcedata html
 data_node = data_demo_html.find(id="data")
 for x in inpress_list + press_list:
     print(x['ID'])
@@ -292,8 +314,48 @@ for x in inpress_list + press_list:
     data_node.append(list_tag)
     br_tag = data_demo_html.new_tag('br')
     data_node.append(br_tag)
+    
+# generate sourcedata html
 with open("sourcedata.html", "w", encoding='utf-8') as file:
     file.write(data_demo_html.decode('utf8'))
+    
+
+# write the highlight paper to highlights html
+highlight_node =  highlights_demo_html.find(id="highlight")
+for x in inpress_list + press_list:
+    print(x['ID'])
+    list_tag = highlights_demo_html.new_tag('li')
+    author = generate_person(x['author'], highlights_demo_html)
+    title = generate_title(x['title'], highlights_demo_html)
+    journal = generate_journal(x['journal'], highlights_demo_html)
+    vol_no_pages = generate_vol_no_page(x, highlights_demo_html)
+    date = generate_date(x, highlights_demo_html)
+    note = generate_note(x, highlights_demo_html)
+    doi_pdf = generate_doi_pdf(x, highlights_demo_html)
+    if not is_highlight(x, highlights_demo_html):  
+        continue    
+
+    list_tag.append(author)
+    # list_tag.append(', ')
+    list_tag.append(title)
+    list_tag.append(' ')
+    list_tag.append(journal)
+    # list_tag.append(', ')
+    list_tag.append(vol_no_pages)
+    # list_tag.append(', ')
+    list_tag.append(date)
+    # list_tag.append('. ')
+    list_tag.append(note)
+    list_tag.append(doi_pdf)
+    
+    highlight_node.append(list_tag)
+    br_tag = highlights_demo_html.new_tag('br')
+    highlight_node.append(br_tag)
+
+# generate highlights html
+with open("highlights.html", "w", encoding='utf-8') as file:
+    file.write(highlights_demo_html.decode('utf8'))
+    
     
 # soup = BeautifulSoup(open('../publications.html', 'r'))
 # inpress_node = soup.find(id="inpress")
